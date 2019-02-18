44d812d3ed7e776433a39445ff8794bc25c7e537
afeiship <1290657123@qq.com>
2019-02-18 19:16:10 +0800

    feat: init & done

---
+++ README.md
@@ -0,0 +1,7 @@
+# next-weapp-auth
+> Authentication for weapp
+
+## install:
+```bash
+npm install -S afeiship/next-weapp-auth --registry=https://registry.npm.taobao.org
+```
---
+++ src/next-weapp-auth.js
@@ -0,0 +1,40 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var nxWxPromisify = nx.wxPromisify || require('next-wx-promisify');
+  var wx = global.wx;
+
+  var NxWeappAuth = nx.declare('nx.WeappAuth', {
+    statics: {
+      auth: function(inOptions) {
+        var self = this;
+        return new Promise(function(resolve) {
+          self.checkSession().then(function(response) {
+            var code = response.code;
+            if (code !== 0) {
+              self.login(inOptions).then(function(response) {
+                resolve(response);
+              });
+            }
+          });
+        });
+      },
+      login: function(inOptions) {
+        return new Promise(function(resolve, reject) {
+          var options = nx.mix(nxWxPromisify(resolve, reject), inOptions);
+          wx.login(options);
+        });
+      },
+      checkSession: function() {
+        return new Promise(function(resolve, reject) {
+          var options = nxWxPromisify(resolve, reject);
+          wx.checkSession(options);
+        });
+      }
+    }
+  });
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxWeappAuth;
+  }
+})();
